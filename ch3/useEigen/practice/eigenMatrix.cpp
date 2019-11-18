2dac7b3f936cea4d0e84180e5080c5c22dc93de5
yejun1204 <yejun1204@gmail.com>
2019-11-18 16:06:27 +0900

    practice dir

---
+++ ch3/useEigen/practice/eigenMatrix.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+#include <ctime>
+#include <eigen3/Eigen/Core>
+//#include <eigen3/Eigen/Dense>
+
+
+#define MATRIX_SIZE 50
+using namespace Eigen;
+
+int main(int argc, char **argv)
+{
+    cout<<"Hello"<<endl;
+    Matrix<float,2,3> matrix_23;
+    Vector3d v_3d;
+    Matrix<float,3,1> vd_3d;
+
+    Matrix3d matrix_33=Matrix3d::Zero();
+
+    
+}
