614e94d9bb8b129913ececeadcf272140f7f07c2
yejun1204 <yejun1204@gmail.com>
2019-11-24 15:49:43 +0900

    eigenGeometry_prac_done

--- ch3/useGeometry/eigenGeometry.cpp
+++ ch3/useGeometry/eigenGeometry.cpp
@@ -26,6 +26,29 @@ int main(int argc, char **argv) {
   v_rotated=rotation_matrix*v;
   cout<<"(1,0,0) after rotation (by matrix)= "<<v_rotated.transpose()<<endl;
   
+  Vector3d euler_angles=rotation_matrix.eulerAngles(2,1,0);
+  cout<<"yaw pitch roll= "<<euler_angles.transpose()<<endl;
+
+  Isometry3d T=Isometry3d::Identity(); //4x4 matrices
+  //setting SE(3)
+  T.rotate(rotation_vector);
+  T.pretranslate(Vector3d(1,3,4));
+
+  cout<<"Transform matrix= \n"<<T.matrix()<<endl;
+
+  Vector3d v_transformed=T*v;
+  cout<<"v transformed= "<<v_transformed.transpose()<<endl;
+
+  Quaterniond q=Quaterniond(rotation_vector);
+  cout<<"quaternion from rotation vector= "<<q.coeffs().transpose()<<endl;
+
+  q=Quaterniond(rotation_matrix);
+  cout<<"quaternion from rotation matrix= "<<q.coeffs().transpose()<<endl;
+
+  v_rotated=q*v;
+  cout<<"(1,0,0) after rotation= "<<v_rotated.transpose()<<endl;
+
+  cout<<"should be equal to "<<(q*Quaterniond(0,1,0,0)*q.inverse()).coeffs().transpose()<<endl;
 
   return 0;
 }
