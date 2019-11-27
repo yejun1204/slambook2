ed1c72226b16b1c4f159f2c030394f0eeaec5ed3
yejun1204 <yejun1204@gmail.com>
2019-11-27 16:19:24 +0900

    ch4 build

--- ch4/example/trajectoryError.cpp
+++ ch4/example/trajectoryError.cpp
@@ -7,8 +7,8 @@
 using namespace Sophus;
 using namespace std;
 
-string groundtruth_file = "./example/groundtruth.txt";
-string estimated_file = "./example/estimated.txt";
+string groundtruth_file = "./groundtruth.txt";
+string estimated_file = "./estimated.txt";
 
 typedef vector<Sophus::SE3d, Eigen::aligned_allocator<Sophus::SE3d>> TrajectoryType;
 
